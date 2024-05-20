cb62fb7858145a3ad459652b883a541a87c13dd7
Jiachang Situ <jiachangsitu@gmail.com>
2024-05-20 15:46:09 -0400

    updated authdetail and added user icon

--- src/Components/auth/AuthDetails.jsx
+++ src/Components/auth/AuthDetails.jsx
@@ -3,9 +3,12 @@ import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { Link } from "react-router-dom";
 import { auth } from '../../firebase/firebase';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Dropdown from 'react-bootstrap/Dropdown';
 import './AuthDetails.css'
+import { BsFillPersonFill } from "react-icons/bs";
 const AuthDetails = () => {
     const [user, setUser] = useState(null);
+    console.log(user);
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (authUser)=>{
             if(authUser){
@@ -27,8 +30,19 @@ const AuthDetails = () => {
             console.log(error);
         })
     }
+
   return (
-    <div className="user-auth-container">{user ? <><button onClick={handleSignOut}> Sign Out</button> </>: 
+    <div className="user-auth-container">{user ?  
+    <Dropdown>
+        <Dropdown.Toggle variant="secondary" id="">
+        <BsFillPersonFill size={20}/>
+          {/* {user.email} */}
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={handleSignOut} href="#">Sign Out</Dropdown.Item>
+        </Dropdown.Menu>
+    </Dropdown>
+    : 
     <div className="login-signup-container">
     <Link to="/login"> <button className="btn btn-outline-primary custom-outline-primary">Log In</button> </Link>
     <Link to="/signup"> <button class="btn btn-primary">Sign Up</button> </Link>
